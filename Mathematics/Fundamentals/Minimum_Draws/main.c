44f37e5e721b28fb84aedc66516f5842abc565be
Alex-TH <alex.trejoh@gmail.com>
2016-03-07 11:30:00 -0600

    Added Minimum_Draws to Mathematics/Fundamentals

---
+++ Mathematics/Fundamentals/Minimum_Draws/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int T;
+    scanf("%d",&T);
+    while(T-- > 0) {
+        int n;
+        scanf("%d",&n);
+        printf("%d\n",n + 1);
+    }
+    return 0;
+}
