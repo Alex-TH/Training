d5e7bf15dd452f6561857e6217a2eccdde3aa31a
Alex-TH <alex.trejoh@gmail.com>
2016-03-09 09:17:36 -0600

    Added Angry Professor to Algortithms/Implementation

---
+++ Algorithms/Implementation/Angry_Professor/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        int n; 
+        int k; 
+        scanf("%d %d",&n,&k);
+        int a[n];
+        for(int a_i = 0; a_i < n; a_i++){
+           scanf("%d",&a[a_i]);
+        }
+    }
+    return 0;
+}
+
